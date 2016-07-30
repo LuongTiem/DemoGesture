4904a0fd4ea1e62b86a871220892f74e34b96c4b
Luong Tiem <luongvantiem@gmail.com>
2016-07-30 11:08:46 +0700

    v1

--- AppMerryChristmas/ViewController.swift
+++ AppMerryChristmas/ViewController.swift
@@ -12,14 +12,63 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+         self.view.backgroundColor = UIColor(red: 17/255, green: 237/255, blue: 252/255, alpha: 1)
+        //--
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.onTap(_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        //--
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.onSwipe(_:)))
+        swipeLeft.direction = .Left
+        self.view.addGestureRecognizer(swipeLeft)
+        //-- 
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.onSwipe(_:)))
+        swipeRight.direction = .Right
+        self.view.addGestureRecognizer(swipeRight)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("run viewdiApper ")
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true //--
+        print("run WillAppear")
+    }
+    
 
+    func onTap(tapGesture : UITapGestureRecognizer){
+        
+        
+        let point = tapGesture.locationInView(self.view)
+        let addBall = UIImageView(image: UIImage(named: "ball"))
+        addBall.bounds.size = CGSizeMake(15, 15)
+        addBall.contentMode = .ScaleAspectFit
+        addBall.center = point
+        self.view.addSubview(addBall)
+        
+        
+    }
+    
+    func onSwipe(swipe: UISwipeGestureRecognizer){
+        if(swipe.direction == .Right){
+            
+            print("vuot phai")
+            
+        }
+        if (swipe.direction == .Left) {
+            print("vuot trai")
+            self.navigationController?.pushViewController((self.storyboard?.instantiateViewControllerWithIdentifier("game") as! Game), animated: true)
+        }
+        
+    }
+    
+    
+    
+  
 
 }
 
